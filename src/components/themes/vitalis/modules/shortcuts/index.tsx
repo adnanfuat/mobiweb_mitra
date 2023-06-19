ebf7bb96bc0effd198d92dda35f6e6d0ebb01575
Tamer UZUN <58685406+adnanfuat@users.noreply.github.com>
2023-06-19 20:00:28 +0300

    asd

--- src/components/themes/vitalis/modules/shortcuts/index.tsx
+++ src/components/themes/vitalis/modules/shortcuts/index.tsx
@@ -1,13 +1,25 @@
 import Image from 'next/image'
+
+
+import { MdCallEnd, MdFacebook, MdEmail } from "react-icons/md";
+
+import { RiInstagramLine, RiFacebookBoxLine, RiMailFill  } from "react-icons/ri";
+
+
+
+
+
 import s from "./index.module.css"    
 
  export   const Shortcuts = () => {
     return (
       <div className={s.shell}>
 
-        Şell...
+          <div className={s.item} style={{color:"#cb06cd"}}><RiInstagramLine size={35}/></div>
+          <div className={s.item} style={{color:"#22529c"}}><RiFacebookBoxLine size={35}/></div>
+          <div className={s.item} style={{color:"#e5b117"}}><RiMailFill size={35}/></div>
                 
-
+          <div className={s.item} style={{marginTop:30}}><MdCallEnd size={40}/></div>
       </div>
     )
   }
